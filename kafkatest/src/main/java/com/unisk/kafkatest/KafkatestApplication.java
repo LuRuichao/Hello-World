f8003a450082aa9585435aea96a3426562f5eed2
陆锐超 <lurc3@chinaunicom.cn>
2018-02-08 16:40:49 +0800

    工程初始化 -----kafkatest

---
+++ kafkatest/src/main/java/com/unisk/kafkatest/KafkatestApplication.java
@@ -0,0 +1,12 @@
+package com.unisk.kafkatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkatestApplication.class, args);
+	}
+}
